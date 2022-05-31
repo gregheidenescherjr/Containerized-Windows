01900fc84b5d1c0d0132f073889b657b560cf198
Gregory Heidenescher Jr <gheidenescher@outlook.com>
2022-05-31 15:16:01 -0600

    Update Enviornments Setup.ps1

--- Enviornments/Enviornments Setup.ps1
+++ Enviornments/Enviornments Setup.ps1
@@ -18,5 +18,5 @@ Write-Host "YourTesting playground is located at G:\"
 Write-Host "This is the first setup in the vision."
 Write Host "The Secure and UnSecure enviornments are only labled as such so that you can tell the difference between what has stored information.
 Write-Host "I would strongly reccomend relocating your user folders such as documents, photos, etc. to G:\"
-Write-Host "This does not hide your IP. THIS DOES NOT HIDE YOUR ONLINE ACTIVITY."
+Write-Host "THIS DOES NOT HIDE YOUR ONLINE ACTIVITY."
 Write-Host "We can limit what information companies and others can see, but lets continue to improve this vision."
