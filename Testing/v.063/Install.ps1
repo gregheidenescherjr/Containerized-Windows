e633b36c9633e8c20251ee0eaa891c7b5e0fe99c
gregheidenescherjr <gheidenescher@outlook.com>
2022-06-24 21:58:48 -0600

    Update

--- Testing/v.063/Install.ps1
+++ Testing/v.063/Install.ps1
@@ -1,39 +1,40 @@
+Write-Host ". 
   ####     ####    ##  ##   ######     ##      ####    ##  ##   ######   #####     ####    ######   ######   ####    
  ##  ##   ##  ##   ### ##     ##      ####      ##     ### ##   ##       ##  ##     ##         ##   ##       ## ##   
  ##       ##  ##   ######     ##     ##  ##     ##     ######   ##       ##  ##     ##        ##    ##       ##  ##  
  ##       ##  ##   ######     ##     ######     ##     ######   ####     #####      ##       ##     ####     ##  ##  
  ##       ##  ##   ## ###     ##     ##  ##     ##     ## ###   ##       ####       ##      ##      ##       ##  ##  
  ##  ##   ##  ##   ##  ##     ##     ##  ##     ##     ##  ##   ##       ## ##      ##     ##       ##       ## ##   
-  ####     ####    ##  ##     ##     ##  ##    ####    ##  ##   ######   ##  ##    ####    ######   ######   ####    
-                                                                                                                     
-						 ##   ##   ####    ##  ##   ####      ####    ##   ##   ####   
-						 ##   ##    ##     ### ##   ## ##    ##  ##   ##   ##  ##  ##  
-						 ##   ##    ##     ######   ##  ##   ##  ##   ##   ##  ##      
-						 ## # ##    ##     ######   ##  ##   ##  ##   ## # ##   ####   
-						 #######    ##     ## ###   ##  ##   ##  ##   #######      ##  
-						 ### ###    ##     ##  ##   ## ##    ##  ##   ### ###  ##  ##  
-						 ##   ##   ####    ##  ##   ####      ####    ##   ##   ####   
+  ####     ####    ##  ##     ##     ##  ##    ####    ##  ##   ######   ##  ##    ####    ######   ######   ####
+				"                                                                                                                     
+Write-Host "		 ##   ##   ####    ##  ##   ####      ####    ##   ##   ####   
+		 ##   ##    ##     ### ##   ## ##    ##  ##   ##   ##  ##  ##  
+		 ##   ##    ##     ######   ##  ##   ##  ##   ##   ##  ##      
+		 ## # ##    ##     ######   ##  ##   ##  ##   ## # ##   ####   
+		 #######    ##     ## ###   ##  ##   ##  ##   #######      ##  
+		 ### ###    ##     ##  ##   ## ##    ##  ##   ### ###  ##  ##  
+		 ##   ##   ####    ##  ##   ####      ####    ##   ##   ####   
 
-									#Containerized Windows
-									#Project Initialized on 5/30/2022 by:
-									#Gregory Heidenescher Jr - Creator/Tester
-									#Christopher Southerland - Alpha Tester
+			#Containerized Windows
+			#Project Initialized on 5/30/2022 by:
+			#Gregory Heidenescher Jr - Creator/Tester
+			#Christopher Southerland - Alpha Tester
 
-#Special Thanks
+# Special Thanks
 #(Sandbox Home Edition) Benny @ https://www.deskmodder.de/blog/2019/04/20/windows-10-home-windows-sandbox-installieren-und-nutzen/
 #(Hyper-V Home Edition) Usman Khurshid @ https://www.itechtics.com/enable-hyper-v-windows-10-home/
 #(Ketarin) Canneverbe @ https://github.com/canneverbe/Ketarin
 #(ScoopBoxManager) LAB02-Research @ https://github.com/LAB02-Research/ScoopBoxManager
 
-						#Tested On:
-						#Edition	Windows 11 Pro
-						#Version	21H2
-						#Installed on	‎2/‎28/‎2022
-						#OS build	22000.708
-						#Experience	Windows Feature Experience Pack 1000.22000.708.0
-						#Processor	AMD Ryzen 9 5900X 12-Core Processor               3.70 GHz
-						#Installed RAM	64.0 GB
-						#System type	64-bit operating system, x64-based processor
+		#Tested On:
+		#Edition	Windows 11 Pro
+		#Version	21H2
+		#Installed on	2/28/2022
+		#OS build	22000.708
+		#Experience	Windows Feature Experience Pack 1000.22000.708.0
+		#Processor	AMD Ryzen 9 5900X 12-Core Processor               3.70 GHz
+		#Installed RAM	64.0 GB
+		#System type	64-bit operating system, x64-based processor
 
 #The main goal is to create a User Experience that exposes as little as possible to the internet while creating a playground for developers.
 #Should anything be compromised, it was all done in a containment area seperate from the main Windows Installation.
@@ -41,19 +42,15 @@
 #will help seperate personal information when browsing the internet through conatinment and compartmentizing applications.
 #We dont need other people to see our credentials during a casual browsing session.
 
-#If you make changes to this, please make it easy to identify by adding your name to the main task you edited. It will be easier later down the road. Thank You.
-#I will later make a word document that will have highlights on recommended user changes.  If a way to simplify, please share.
+#If you make changes to this, please make it easy to identify by adding your name to the main task you edited.
+It will be easier later down the road. Thank You.
+#I will later make a word document that will have highlights on recommended user changes.
+If a way to simplify, please share."
 
 PowerShell -NoProfile -ExecutionPolicy "Unrestricted" -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy "Unrestricted" -File "".\Install.ps1""'-Verb RunAs}";
 
 #Set Directory to PSScriptRoot
 if ((Get-Location).Path -NE $PSScriptRoot) { Set-Location $PSScriptRoot }
-
-#Copying Required Files
-Copy-Item ".\Secure Internet.wsb" -Destination "C:\Users\Public\Documents"
-Copy-Item ".\UnSecure Internet.wsb" -Destination "C:\Users\Public\Documents"
-Copy-Item ".\Install.ps1" -Destination "C:\Users\Public\Documents"
-Remove-Item ".\Shortcuts"
 
 #Root Account Setup
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Description."
@@ -131,7 +128,8 @@ New-Item -FilePath "H:\Documents" -itemType Directory
 New-Item -FilePath "H:\Picutres" -itemType Directory
 New-Item -FilePath "H:\Downloads" -itemType Directory
 
-
+Copy-Item ".\Secure Internet.wsb" -Destination "C:\Users\Public\Documents"
+Copy-Item ".\UnSecure Internet.wsb" -Destination "C:\Users\Public\Documents"
 
 Write-Host "Shortcuts and Directories Enabled" -foregroundcolor "Green"
 }
@@ -170,12 +168,11 @@ switch ($rslt) {
 0{
 Push-Location $PSScriptRoot
 #Cleanup
-Remove-Item ".\Containerize\Scripts"
-Remove-Item "C:\Users\Public\Documents\Install.ps1"
 
 #AutoMount Drives At Startup
 Register-ScheduledTask -xml (Get-Content "C:\Users\Public\Documents\AutoMountVDrives.xml" | Out-String) -TaskName "AutoMountDrives" -TaskPath "C:\Windows\System32\Tasks" –Force
-Write-Host "Sandbox User Folders: %PROGRAMDATA%\Microsoft\Windows\Containers\<GUID>\BaseLayer\Files\Users\WDAGUtilityAccount\Documents"
+
+Write-Host "Sandbox User Folders: %PROGRAMDATA%\Microsoft\Windows\Containers\<GUID>\BaseLayer\Files\Users\WDAGUtilityAccount\Documents""
 pause
 Restart-Computer
 }
@@ -184,7 +181,7 @@ Restart-Computer
 
 
 
-<#Things I might use later...
+#Things I might use later...
 
 #Remove Apps
 #Get-AppXPackage *bingweather* | Remove-AppXPackage
@@ -200,11 +197,4 @@ Restart-Computer
 
 #ForEach($app in $appname){
 #Get-AppxProvisionedPackage -Online | where {$_.PackageName -like $app} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
-
-#%PROGRAMDATA%\Microsoft\Windows\Containers\<GUID>\BaseLayer\Files\Users\WDAGUtilityAccount\Documents
-
-
-#PowerShell -Command "Set-ExecutionPolicy RemoteSigned -scope Process; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; iwr -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | iex"
-
-
-#}>
+#}
